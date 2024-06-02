c84f2b295b6d5dade10a9deb7c9a7730221a9734
calinik <nikhil10443@gmail.com>
2024-06-02 12:34:22 +0530

    First Files

---
+++ index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("nikhilrana");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1>Please Log in at chai aur code </h1>");
+});
+
+app.get("/youtube", (req, res) => {
+  res.send("<h2>hello youtube</h2>");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on ${process.env.PORT}`);
+});
